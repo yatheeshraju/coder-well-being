ac475ea95540e383060db391a995d402309edc52
Yatheesh Konduru <yatheesh.konduru@gmail.com>
2019-07-28 14:47:08 +0530

    added few task

--- renderer.js
+++ renderer.js
@@ -6,7 +6,11 @@ function setTask() {
     var rems = [
         { task: 'blink your eyes ', icon: '<i class="em em-eyes"></i> ' },
         { task: 'take a walk ', icon: '<i class="em em-man-running"></i> ' },
-        { task: 'standup and stretch', icon: ' <i class="em em-man-lifting-weights"></i> ' }
+        { task: 'standup and stretch', icon: ' <i class="em em-man-lifting-weights"></i> ' },
+        { task: 'have a sip of water', icon: ' <i class="em em-potable_water"></i> ' },
+        { task: 'stand up and touch your toes', icon: ' <i class="em em-man_dancing"></i>' },
+        { task: 'do some situps ', icon: ' <i class="em em-man-lifting-weights"></i> ' },
+        { task: 'turn your head in circle a few times', icon: ' <i class="em em-man-gesturing-ok" ></i> ' }
     ]
     let randomTaskjson = rems[Math.floor(Math.random() * rems.length)]
 
